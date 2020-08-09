6b1690811b06ffbae7b3cdd752968a0a82d95915
RediGt <red.i@inbox.lv>
2020-08-09 19:16:03 +0300

    Add project files.

---
+++ TicTacToeWinF/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TicTacToeWinF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblPanelGrid = new System.Windows.Forms.TableLayoutPanel();
+            this.pctBox1 = new System.Windows.Forms.PictureBox();
+            this.pctBox2 = new System.Windows.Forms.PictureBox();
+            this.pctBox3 = new System.Windows.Forms.PictureBox();
+            this.pctBox4 = new System.Windows.Forms.PictureBox();
+            this.pctBox5 = new System.Windows.Forms.PictureBox();
+            this.pctBox6 = new System.Windows.Forms.PictureBox();
+            this.pctBox7 = new System.Windows.Forms.PictureBox();
+            this.pctBox8 = new System.Windows.Forms.PictureBox();
+            this.pctBox9 = new System.Windows.Forms.PictureBox();
+            this.tblPanelGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblPanelGrid
+            // 
+            this.tblPanelGrid.ColumnCount = 3;
+            this.tblPanelGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tblPanelGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tblPanelGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tblPanelGrid.Controls.Add(this.pctBox9, 2, 2);
+            this.tblPanelGrid.Controls.Add(this.pctBox8, 1, 2);
+            this.tblPanelGrid.Controls.Add(this.pctBox7, 0, 2);
+            this.tblPanelGrid.Controls.Add(this.pctBox6, 2, 1);
+            this.tblPanelGrid.Controls.Add(this.pctBox5, 1, 1);
+            this.tblPanelGrid.Controls.Add(this.pctBox4, 0, 1);
+            this.tblPanelGrid.Controls.Add(this.pctBox3, 2, 0);
+            this.tblPanelGrid.Controls.Add(this.pctBox2, 1, 0);
+            this.tblPanelGrid.Controls.Add(this.pctBox1, 0, 0);
+            this.tblPanelGrid.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tblPanelGrid.Location = new System.Drawing.Point(0, 0);
+            this.tblPanelGrid.Name = "tblPanelGrid";
+            this.tblPanelGrid.RowCount = 3;
+            this.tblPanelGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tblPanelGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tblPanelGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tblPanelGrid.Size = new System.Drawing.Size(600, 601);
+            this.tblPanelGrid.TabIndex = 0;
+            // 
+            // pctBox1
+            // 
+            this.pctBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox1.Location = new System.Drawing.Point(3, 3);
+            this.pctBox1.Name = "pctBox1";
+            this.pctBox1.Size = new System.Drawing.Size(193, 194);
+            this.pctBox1.TabIndex = 0;
+            this.pctBox1.TabStop = false;
+            // 
+            // pctBox2
+            // 
+            this.pctBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox2.Location = new System.Drawing.Point(202, 3);
+            this.pctBox2.Name = "pctBox2";
+            this.pctBox2.Size = new System.Drawing.Size(193, 194);
+            this.pctBox2.TabIndex = 1;
+            this.pctBox2.TabStop = false;
+            // 
+            // pctBox3
+            // 
+            this.pctBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox3.Location = new System.Drawing.Point(401, 3);
+            this.pctBox3.Name = "pctBox3";
+            this.pctBox3.Size = new System.Drawing.Size(196, 194);
+            this.pctBox3.TabIndex = 2;
+            this.pctBox3.TabStop = false;
+            // 
+            // pctBox4
+            // 
+            this.pctBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox4.Location = new System.Drawing.Point(3, 203);
+            this.pctBox4.Name = "pctBox4";
+            this.pctBox4.Size = new System.Drawing.Size(193, 194);
+            this.pctBox4.TabIndex = 3;
+            this.pctBox4.TabStop = false;
+            // 
+            // pctBox5
+            // 
+            this.pctBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox5.Location = new System.Drawing.Point(202, 203);
+            this.pctBox5.Name = "pctBox5";
+            this.pctBox5.Size = new System.Drawing.Size(193, 194);
+            this.pctBox5.TabIndex = 4;
+            this.pctBox5.TabStop = false;
+            // 
+            // pctBox6
+            // 
+            this.pctBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox6.Location = new System.Drawing.Point(401, 203);
+            this.pctBox6.Name = "pctBox6";
+            this.pctBox6.Size = new System.Drawing.Size(196, 194);
+            this.pctBox6.TabIndex = 5;
+            this.pctBox6.TabStop = false;
+            // 
+            // pctBox7
+            // 
+            this.pctBox7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox7.Location = new System.Drawing.Point(3, 403);
+            this.pctBox7.Name = "pctBox7";
+            this.pctBox7.Size = new System.Drawing.Size(193, 195);
+            this.pctBox7.TabIndex = 6;
+            this.pctBox7.TabStop = false;
+            // 
+            // pctBox8
+            // 
+            this.pctBox8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox8.Location = new System.Drawing.Point(202, 403);
+            this.pctBox8.Name = "pctBox8";
+            this.pctBox8.Size = new System.Drawing.Size(193, 195);
+            this.pctBox8.TabIndex = 7;
+            this.pctBox8.TabStop = false;
+            // 
+            // pctBox9
+            // 
+            this.pctBox9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctBox9.Location = new System.Drawing.Point(401, 403);
+            this.pctBox9.Name = "pctBox9";
+            this.pctBox9.Size = new System.Drawing.Size(196, 195);
+            this.pctBox9.TabIndex = 8;
+            this.pctBox9.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 601);
+            this.Controls.Add(this.tblPanelGrid);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.tblPanelGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblPanelGrid;
+        private System.Windows.Forms.PictureBox pctBox9;
+        private System.Windows.Forms.PictureBox pctBox8;
+        private System.Windows.Forms.PictureBox pctBox7;
+        private System.Windows.Forms.PictureBox pctBox6;
+        private System.Windows.Forms.PictureBox pctBox5;
+        private System.Windows.Forms.PictureBox pctBox4;
+        private System.Windows.Forms.PictureBox pctBox3;
+        private System.Windows.Forms.PictureBox pctBox2;
+        private System.Windows.Forms.PictureBox pctBox1;
+    }
+}
+
